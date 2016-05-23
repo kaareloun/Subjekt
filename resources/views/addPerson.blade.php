6ff707a181d300d8750cb58c9133e3d0005468b8
Sander Sadam <sandersadam1@gmail.com>
2016-05-23 15:15:55 +0300

    aad
    dds

--- resources/views/addPerson.blade.php
+++ resources/views/addPerson.blade.php
@@ -11,8 +11,8 @@
             <div class="content">
                 <form class="" action="create" method="post">
                     <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
-                    first name: <input type="text" name="first_name">{{$errors->first('first_name')}}<br>
-                    last name: <input type="text" name="last_name">{{$errors->first('last_name')}}<br>
+                    first name: <input value="{{old('first_name')}}" type="text" name="first_name">{{$errors->first('first_name')}}<br>
+                    last name: <input value="{{old('last_name')}}" type="text" name="last_name">{{$errors->first('last_name')}}<br>
                     identity_code: <input type="text" name="identity_code"><br>
                     birth_date: <input type="text" name="birth_date"><br>
                     -----<br>
