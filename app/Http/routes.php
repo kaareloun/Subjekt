456b31e79549cc3cbb0b371e17961aec508daee9
Kaarel Õun <kaareloun@gmail.com>
2016-05-23 11:31:23 +0300

    enterprise algus

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,9 +16,16 @@ Route::get('/', function () {
     return view('welcome', compact('employee'));
 });
 
+//PERSON
 Route::get('/person/create', function () {
     return view('addPerson');
 });
-
 Route::get('/person/{id}', 'PersonController@show');
 Route::post('/person/{id}', 'PersonController@store');
+
+//ENTERPRISE
+Route::get('/enterprise/create', function () {
+    return view('addEnterprise');
+});
+Route::get('/enterprise/{id}', 'EnterpriseController@show');
+Route::post('/enterprise/{id}', 'EnterpriseController@store');
---
+++ resources/views/addEnterprise.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <form class="" action="enterprise/create.php" method="post">
+                    <input type="text" name="name" placeholder="Name">
+                    <input type="text" name="full_name" placeholder="Full name">
+                </form>
+
+            </div>
+        </div>
+    </body>
+</html>
